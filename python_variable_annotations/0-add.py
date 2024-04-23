88ec3042b03b80142bec594c853a0e7fc2bfbb0c
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-04-23 21:52:49 +0200

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+type-annotated function add that takes a float a and a float b as arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """returns the sum as a float"""
+    return a + b
