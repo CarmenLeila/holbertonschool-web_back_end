3ed9adfb551f0c5b04a17b6fe21fe423423ffff0
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-04-24 12:29:47 +0200

    fixing bugs

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -8,6 +8,6 @@ input_list of floats as argument and returns their sum as a float
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return sum of floats"""
     return sum(input_list)
