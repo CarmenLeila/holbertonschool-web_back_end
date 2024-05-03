04a32bdce0cb8b9c40368c1396e95b6084cd3545
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-05-03 04:22:04 +0200

    0. Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""function named index_range that takes two integer arguments page and page_size"""
+"""takes two integer arguments page and page_size"""
 
 
 def index_range(page, page_size):
-"""The function should return a tuple of size two containing a start index"""
-start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    """returns a tuple of size two containing a start index"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
     return (start_index, end_index)
