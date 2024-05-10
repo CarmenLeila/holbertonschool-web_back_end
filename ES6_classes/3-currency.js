1840a18c7b520b5c85d188fc7f782f71805e8985
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-05-10 22:13:37 +0200

    fixing bugs

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,40 +1,40 @@
 export default class Currency {
-    constructor(code, name) {
-      if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._code = code;
-      this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
-  
-    get code() {
-      return this._code;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._code = code;
+    this._name = name;
+  }
   
-    set code(newCode) {
-      if (typeof newCode !== 'string') {
-        throw new TypeError('Code must be a string');
-      } else {
-        this._code = newCode;
-      }
-    }
+  get code() {
+    return this._code;
+  }
   
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
-      if (typeof newName !== 'string') {
-        throw new TypeError('Name must be a string');
-      } else {
-        this._name = newName;
-      }
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = newCode;
     }
   }
+  
+  get name() {
+    return this._name;
+  }
+  
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = newName;
+    }
+  }
+  
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
