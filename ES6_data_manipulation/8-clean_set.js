bd356b057a9018f2026632d3c6648b6aff56e422
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-05-29 23:44:58 +0200

    function named cleanSet that returns a string of all the set values

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
   const result = [];
   set.forEach((value) => {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   });
